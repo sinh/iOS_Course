89e8d24e9d34931f4600308e0c8c1aa32189d8e2
sinh <sinhnx@fpt.aptech.ac.vn>
2013-09-26 15:59:01 +0700

    Initial Commit
    IAD Session01 create C_Project

---
+++ Session01/C_Project/C_Project/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  C_Project
+//
+//  Created by Nguyen Xuan Sinh on 9/26/13.
+//  Copyright (c) 2013 FPT Aptech. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
